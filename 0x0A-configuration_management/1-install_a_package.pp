f9a234887b0c2c95ef36be9a9103a07589258677
Salomon700 <ntwalisolomon9@gmail.com>
2024-08-04 11:19:39 +0200

    fixed task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-# Installs a package
+# 1-install_a_package.pp
 
-exec { 'flask':
-  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
+# Ensure pip3 is installed first (optional, adjust as necessary for your system)
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep "Version: 2.1.0"',
 }
